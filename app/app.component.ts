324aba096fe6aaa4fe9de36884907fc139189ea0
andrei.diaconescu <andrei.diaconescu@anais.digital>
2016-08-25 15:31:39 +0300

    following angular.io/docs/ts/latest/tutorial/toh-pt5.html, before adding routing; fixed a missing import in the tutorial: The HeroDetailComponent had not been imported and added to the declaration array (found on stackoverflow)

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+import {HeroesComponent} from './heroes.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+    <h1>{{title}}</h1>
+    <my-heroes></my-heroes>
+  `
 })
-export class AppComponent { }
+export class AppComponent {
+    title = 'Tour of Heroes';
+}
